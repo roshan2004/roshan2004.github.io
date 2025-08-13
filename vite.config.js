9ddc86e70eec5e332bc5b17bc66961cfb987879e
Roshan Shrestha <roshanpra@gmail.com>
2025-08-13 23:48:40 +0200

    Pages deploy via Actions

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/', // user site -> root path
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
