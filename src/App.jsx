0408d7194e94e5ab3624dba59a349447b8e539c0
Roshan Shrestha <roshanpra@gmail.com>
2025-08-14 14:32:45 +0200

    Minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,16 @@ import Research from './components/Research';
 import Publications from './components/Publications';
 import Teaching from './components/Teaching';
 import Projects from './components/Projects';
+import Blog from './components/Blog';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './App.css';
 
+/*
+ * Root application component.  Maintains the currently active section and
+ * renders the corresponding component.  A new "blog" case has been added
+ * to handle the blog section introduced for sharing posts and updates.
+ */
 function App() {
   const [activeSection, setActiveSection] = useState('home');
 
@@ -27,6 +33,8 @@ function App() {
         return <Teaching />;
       case 'projects':
         return <Projects />;
+      case 'blog':
+        return <Blog />;
       case 'contact':
         return <Contact />;
       default:
@@ -37,9 +45,7 @@ function App() {
   return (
     <div className="min-h-screen bg-slate-50">
       <Header activeSection={activeSection} setActiveSection={setActiveSection} />
-      <main>
-        {renderSection()}
-      </main>
+      <main>{renderSection()}</main>
       <Footer />
     </div>
   );
