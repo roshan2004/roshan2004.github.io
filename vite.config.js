13ef0b161563785264dff4a9b218a0d57c097c98
Roshan Shrestha <roshanpra@gmail.com>
2025-08-19 10:27:43 +0200

    Added updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   base: '/', // root path for GitHub Pages with custom domain
