359d8f6dee27f95fe2786b49a04102b3d1d652c0
Roshan Shrestha <roshanpra@gmail.com>
2025-08-19 10:38:50 +0200

    Revert "Added updates"
    This reverts commit 2b882245a56994c671af3d904a1a2a7f70e5fedc.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+import path from 'path'
 
 export default defineConfig({
   base: '/', // root path for GitHub Pages with custom domain
